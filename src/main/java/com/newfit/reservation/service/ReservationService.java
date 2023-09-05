c81c4495fc11c11ab37a8fc8b1bd24f0cf69261e
leesh <qwqw8019@naver.com>
2023-09-06 01:52:00 +0900

    feat: add Reservation service (#3)

---
+++ src/main/java/com/newfit/reservation/service/ReservationService.java
@@ -0,0 +1,15 @@
+package com.newfit.reservation.service;
+
+
+import com.newfit.reservation.repository.ReservationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+}
