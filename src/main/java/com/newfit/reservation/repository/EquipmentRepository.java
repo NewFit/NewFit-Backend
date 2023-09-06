10f5343a3038cdc6231c8d0f5dd41ac62c04698b
leesh <qwqw8019@naver.com>
2023-09-06 14:52:53 +0900

    feat: correct ID type in EquipmentRepository (#3)

--- src/main/java/com/newfit/reservation/repository/EquipmentRepository.java
+++ src/main/java/com/newfit/reservation/repository/EquipmentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EquipmentRepository extends JpaRepository<Equipment, Integer> {
+public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
 }
