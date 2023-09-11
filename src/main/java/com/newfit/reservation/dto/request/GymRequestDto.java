ee5e6ab5af7817711f37fa5a0bc2f66727ee4794
leesh <qwqw8019@naver.com>
2023-09-12 02:42:00 +0900

    feat: GymRequestDto 구현 (#24)

---
+++ src/main/java/com/newfit/reservation/dto/request/GymRequestDto.java
@@ -0,0 +1,10 @@
+package com.newfit.reservation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GymRequestDto {
+    private Long gymId;
+}
