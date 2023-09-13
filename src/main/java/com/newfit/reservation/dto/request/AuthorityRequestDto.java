94d100a210d5e2ab500b9245ed1378ebc12c9bf6
leesh <qwqw8019@naver.com>
2023-09-13 14:00:42 +0900

    rename: requestDto 이름 변경 (#24)
    GymRequestDto.java ->
    AuthorityRequestDto.java

---
+++ src/main/java/com/newfit/reservation/dto/request/AuthorityRequestDto.java
@@ -0,0 +1,16 @@
+package com.newfit.reservation.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AuthorityRequestDto {
+    @NotNull
+    private Long gymId;
+}
