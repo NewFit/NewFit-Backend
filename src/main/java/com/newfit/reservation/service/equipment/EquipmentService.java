4ff917d8955369a4c3a72bec8913dbc493874ea6
chosw1002@naver.com <chosw1002@naver.com>
2023-09-13 09:50:17 +0900

    feat: Equipment를 새로 등록하는 service 구현 (#22)

--- src/main/java/com/newfit/reservation/service/equipment/EquipmentService.java
+++ src/main/java/com/newfit/reservation/service/equipment/EquipmentService.java
@@ -1,13 +1,36 @@
 package com.newfit.reservation.service.equipment;
 
+import com.newfit.reservation.domain.Gym;
+import com.newfit.reservation.domain.equipment.Equipment;
+import com.newfit.reservation.domain.equipment.Purpose;
 import com.newfit.reservation.repository.equipment.EquipmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class EquipmentService {
     private final EquipmentRepository equipmentRepository;
+
+    /*
+    Equipment를 새로 등록.
+    이미 존재한다면 등록없이 반환.
+     */
+    public Equipment registerEquipment(Gym gym, String name, Purpose purpose) {
+        if (doesExists(gym, name, purpose)) {
+            return equipmentRepository.findByGymAndNameAndPurpose(gym, name, purpose).get();
+        }
+        return equipmentRepository.save(Equipment.createEquipment(gym, name, purpose));
+    }
+
+    /*
+    registerEquipment 메서드에서 이미 존재하는 메서드인지 확인하기 위해 호출
+     */
+    public boolean doesExists(Gym gym, String name, Purpose purpose) {
+        // pk를 제외한 모든 것이 일치하는지 boolean으로 반환
+        return equipmentRepository.findByGymAndNameAndPurpose(gym, name, purpose).isPresent();
+    }
 }
