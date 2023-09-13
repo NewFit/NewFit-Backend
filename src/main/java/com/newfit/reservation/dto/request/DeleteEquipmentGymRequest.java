2a7628dcec8efb2cad44f6347ede4094398b10c3
chosw1002@naver.com <chosw1002@naver.com>
2023-09-13 18:51:39 +0900

    feat: Staff1의 delete 기능을 위한 request Dto 생성 (#22)

---
+++ src/main/java/com/newfit/reservation/dto/request/DeleteEquipmentGymRequest.java
@@ -0,0 +1,11 @@
+package com.newfit.reservation.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+
+@Getter
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class DeleteEquipmentGymRequest {
+    private Long equipmentGymId;
+}
---
+++ src/main/java/com/newfit/reservation/dto/request/DeleteEquipmentRequest.java
@@ -0,0 +1,16 @@
+package com.newfit.reservation.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class DeleteEquipmentRequest {
+    private Long equipmentId;
+}
