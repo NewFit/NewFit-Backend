b75a518f42a68d866cf9bd5a836a714f3dd0711d
chosw1002@naver.com <chosw1002@naver.com>
2023-09-13 09:52:18 +0900

    feat: 입력받은 count만큼 등록 (#22)

--- src/main/java/com/newfit/reservation/service/equipment/EquipmentGymService.java
+++ src/main/java/com/newfit/reservation/service/equipment/EquipmentGymService.java
@@ -1,9 +1,13 @@
 package com.newfit.reservation.service.equipment;
 
+import com.newfit.reservation.domain.Gym;
+import com.newfit.reservation.domain.equipment.Equipment;
+import com.newfit.reservation.domain.equipment.EquipmentGym;
 import com.newfit.reservation.repository.equipment.EquipmentGymRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +15,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class EquipmentGymService {
 
     private final EquipmentGymRepository equipmentGymRepository;
+
+    /*
+    입력받은 개수(count)만큼 등록
+     */
+    public void registerEquipmentInGym(Equipment equipment, Gym gym, Integer count) {
+        IntStream.range(0, count)
+                .forEach(repeat -> equipmentGymRepository.save(EquipmentGym.createEquipmentGym(equipment, gym)));
+    }
 }
