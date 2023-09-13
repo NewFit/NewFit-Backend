0996a954382af9446ae804980072225fd1648660
chosw1002@naver.com <chosw1002@naver.com>
2023-09-13 10:07:17 +0900

    feat: Staff1_List1을 위한 EquipmentGym의 service와 repository 구현 (#22)

--- src/main/java/com/newfit/reservation/repository/equipment/EquipmentGymRepository.java
+++ src/main/java/com/newfit/reservation/repository/equipment/EquipmentGymRepository.java
@@ -1,10 +1,14 @@
 package com.newfit.reservation.repository.equipment;
 
 
+import com.newfit.reservation.domain.Gym;
 import com.newfit.reservation.domain.equipment.EquipmentGym;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EquipmentGymRepository extends JpaRepository<EquipmentGym, Long> {
+    List<EquipmentGym> findAllByGym(Gym gym);
 }
--- src/main/java/com/newfit/reservation/service/equipment/EquipmentGymService.java
+++ src/main/java/com/newfit/reservation/service/equipment/EquipmentGymService.java
@@ -3,10 +3,15 @@ package com.newfit.reservation.service.equipment;
 import com.newfit.reservation.domain.Gym;
 import com.newfit.reservation.domain.equipment.Equipment;
 import com.newfit.reservation.domain.equipment.EquipmentGym;
+import com.newfit.reservation.dto.response.EquipmentGymListResponse;
+import com.newfit.reservation.dto.response.EquipmentResponse;
 import com.newfit.reservation.repository.equipment.EquipmentGymRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Service
@@ -22,5 +27,20 @@ public class EquipmentGymService {
     public void registerEquipmentInGym(Equipment equipment, Gym gym, Integer count) {
         IntStream.range(0, count)
                 .forEach(repeat -> equipmentGymRepository.save(EquipmentGym.createEquipmentGym(equipment, gym)));
+    }
+
+    /*
+    Gym으로 EquipmentGym을 모두 조회
+    Response를 위한 Dto에 담아 반환
+     */
+    public EquipmentGymListResponse findAllInGym(Gym gym) {
+        List<EquipmentGym> allByGym = equipmentGymRepository.findAllByGym(gym);
+
+        List<EquipmentResponse> equipmentResponses = allByGym.stream()
+                .map(e -> new EquipmentResponse(e.getId(), e.getEquipment().getName(), e.getEquipment().getPurpose(), e.getCondition()))
+                .collect(Collectors.toList());
+
+        EquipmentGymListResponse response = new EquipmentGymListResponse(gym.getName(), allByGym.size(), equipmentResponses);
+        return response;
     }
 }
