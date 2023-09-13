0f381a9f098b80960e9b2f07df0a8a202827607d
chosw1002@naver.com <chosw1002@naver.com>
2023-09-13 09:46:13 +0900

    feat: repository가 반환한 Authority의 Gym 반환 (#22)

--- src/main/java/com/newfit/reservation/service/AuthorityService.java
+++ src/main/java/com/newfit/reservation/service/AuthorityService.java
@@ -1,5 +1,6 @@
 package com.newfit.reservation.service;
 
+import com.newfit.reservation.domain.Gym;
 import com.newfit.reservation.repository.AuthorityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,4 +12,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthorityService {
 
     private final AuthorityRepository authorityRepository;
+
+    /*
+    userId로 조회
+    repository가 반환한 Authority의 Gym 반환
+     */
+    public Gym getGym(Long userId) {
+        return authorityRepository.findOneByUserId(userId).get().getGym();
+    }
 }
