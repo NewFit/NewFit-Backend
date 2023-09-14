93a51fdb4f76242a84fccd2b8b6b4167b3600568
chosw1002@naver.com <chosw1002@naver.com>
2023-09-14 11:52:27 +0900

    refactor: request Dto에 기본 생성자 추가 및 validation 적용 (#22)

--- src/main/java/com/newfit/reservation/dto/request/DeleteEquipmentRequest.java
+++ src/main/java/com/newfit/reservation/dto/request/DeleteEquipmentRequest.java
@@ -1,16 +1,16 @@
 package com.newfit.reservation.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class DeleteEquipmentRequest {
+    @NonNull
     private Long equipmentId;
 }
--- src/main/java/com/newfit/reservation/dto/request/RegisterEquipmentRequest.java
+++ src/main/java/com/newfit/reservation/dto/request/RegisterEquipmentRequest.java
@@ -1,15 +1,24 @@
 package com.newfit.reservation.dto.request;
 
 import com.newfit.reservation.domain.equipment.Purpose;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RegisterEquipmentRequest {
+    @NotBlank
     private String name;
+
+    @NotBlank
     private Purpose purpose;
+
+    @NotNull
+    @Min(value = 1, message = "1개 이상의 기구를 등록해야 합니다.")
     private Integer count;
 }
