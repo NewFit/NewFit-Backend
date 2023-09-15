df2cd97e2868e2c776ee6cc100b848ba9c7dd871
hoyun06 <hoyun06@gmail.com>
2023-09-15 12:29:13 +0900

    feat: GYM1_LIST1을 위한 getAllGyms 메소드를 GymService.java에 추가. (#25)

--- src/main/java/com/newfit/reservation/service/GymService.java
+++ src/main/java/com/newfit/reservation/service/GymService.java
@@ -1,11 +1,15 @@
 package com.newfit.reservation.service;
 
 import com.newfit.reservation.domain.Gym;
+import com.newfit.reservation.dto.response.GymListResponse;
+import com.newfit.reservation.dto.response.GymResponseDto;
 import com.newfit.reservation.repository.GymRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +22,21 @@ public class GymService {
 
     public Gym findById(Long gymId) {
         return gymRepository.findById(gymId).orElseThrow(IllegalArgumentException::new);
+    }
+
+    /*
+    GymRepository로부터 NewFit 서비스에 등록된 모든 헬스장을 list 형태로 받습니다.
+    그 다음에 Dto로 변환하여 Controller에게 반환합니다.
+     */
+    public GymListResponse getAllGyms() {
+        List<Gym> findGyms = gymRepository.findAll();
+
+        List<GymResponseDto> gyms = findGyms.stream()
+                .map(GymResponseDto::new)
+                .collect(Collectors.toList());
+
+        return GymListResponse.builder()
+                .gyms(gyms)
+                .build();
     }
 }
