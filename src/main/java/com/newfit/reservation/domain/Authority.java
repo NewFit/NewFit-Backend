c5f48a55357f7ab33d344ee41c94423764ee53d0
hoyun06 <hoyun06@gmail.com>
2023-09-15 15:43:56 +0900

    feat: Authority.java에 accepted 필드값을 업데이트하는 메소드 추가 (#23)

--- src/main/java/com/newfit/reservation/domain/Authority.java
+++ src/main/java/com/newfit/reservation/domain/Authority.java
@@ -44,6 +44,11 @@ public class Authority extends BaseTimeEntity {
     @OneToMany(mappedBy = "authority", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Credit> creditList = new ArrayList<>();
 
+    // accepted 필드값을 업데이트하는 메소드입니다.
+    public void updateAccepted(boolean accepted) {
+        this.accepted = accepted;
+    }
+
     //========= 연관관계 편의 메소드입니다. =========//
     public void setUserRelation(User user) {
         this.user = user;
