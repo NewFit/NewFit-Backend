4e75884c4f4e26cdc2da03862c13d3039cd2a9a9
hoyun06 <hoyun06@gmail.com>
2023-09-16 21:10:38 +0900

    feat: USER6_ROUTINE1 기능 구현을 위해 UserService.java에 findOneById 메소드 작성 (#34)

--- src/main/java/com/newfit/reservation/service/UserService.java
+++ src/main/java/com/newfit/reservation/service/UserService.java
@@ -1,9 +1,12 @@
 package com.newfit.reservation.service;
 
+import com.newfit.reservation.domain.User;
 import com.newfit.reservation.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public Optional<User> findOneById(Long userId) {
+        return userRepository.findOne(userId);
+    }
 }
