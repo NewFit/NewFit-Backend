7c0dbc159a04c626a8214478cac708333ae49cab
leesh <qwqw8019@naver.com>
2023-09-17 03:56:46 +0900

    feat: 예약 생성 기능 구현(#35)

--- src/main/java/com/newfit/reservation/service/reservation/ReservationService.java
+++ src/main/java/com/newfit/reservation/service/reservation/ReservationService.java
@@ -1,10 +1,21 @@
 package com.newfit.reservation.service.reservation;
 
 
+import com.newfit.reservation.domain.Gym;
+import com.newfit.reservation.domain.User;
+import com.newfit.reservation.domain.equipment.EquipmentGym;
+import com.newfit.reservation.domain.reservation.Reservation;
+import com.newfit.reservation.dto.request.ReservationRequest;
+import com.newfit.reservation.dto.response.ReservationResponse;
+import com.newfit.reservation.repository.AuthorityRepository;
+import com.newfit.reservation.repository.UserRepository;
+import com.newfit.reservation.repository.equipment.EquipmentGymRepository;
 import com.newfit.reservation.repository.reservation.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -12,4 +23,52 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
+    private final UserRepository userRepository;
+    private final AuthorityRepository authorityRepository;
+    private final EquipmentGymRepository equipmentGymRepository;
+
+
+    private void validateReserverRegistration(Long userId, Gym gym) {
+
+        authorityRepository.findAuthoritiesByUserId(userId)
+                .stream().parallel()
+                .filter(a -> a.getGym()
+                        .equals(gym))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public ReservationResponse reserve(Long userId,
+                                       Long equipmentId,
+                                       ReservationRequest request) {
+
+        User reserver = userRepository.findOne(userId)
+                .orElseThrow(IllegalArgumentException::new);
+
+        // 사용 가능한 기구 하나를 가져옴
+        EquipmentGym usedEquipment = equipmentGymRepository.findAvailableByEquipmentId(equipmentId)
+                .stream()
+                .findFirst()
+                .orElseThrow(()-> new NoSuchElementException("There is no available equipment"));
+
+
+        // 사용자가 헬스장에 등록되어 있는지 확인
+        validateReserverRegistration(userId, usedEquipment.getGym());
+
+
+        usedEquipment.use();
+        equipmentGymRepository.save(usedEquipment);
+
+        Reservation reservation = Reservation.builder()
+                .reserver(reserver)
+                .equipmentGym(usedEquipment)
+                .startAt(request.getStartAt())
+                .endAt(request.getEndAt())
+                .repetitionNumber(request.getRepetitionNumber())
+                .build();
+
+        Reservation result = reservationRepository.save(reservation);
+
+        return new ReservationResponse(result.getId());
+    }
 }
