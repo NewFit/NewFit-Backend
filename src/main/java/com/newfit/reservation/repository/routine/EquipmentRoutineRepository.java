a21209ef6108b2694dde6cccb972b87e691459e4
hoyun06 <hoyun06@gmail.com>
2023-09-16 23:13:46 +0900

    feat: USER6_ROUTINE2 기능 구현을 위해 EquipmentRoutineRepository.java에 deleteAllByRoutine 메소드 작성 (#34)

--- src/main/java/com/newfit/reservation/repository/routine/EquipmentRoutineRepository.java
+++ src/main/java/com/newfit/reservation/repository/routine/EquipmentRoutineRepository.java
@@ -1,9 +1,13 @@
 package com.newfit.reservation.repository.routine;
 
 import com.newfit.reservation.domain.routine.EquipmentRoutine;
+import com.newfit.reservation.domain.routine.Routine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EquipmentRoutineRepository extends JpaRepository<EquipmentRoutine, Long> {
+
+    // 특정 Routine에 묶여있는 모든 EquipmentRoutine 객체를 삭제하는 메소드입니다.
+    void deleteAllByRoutine(Routine routine);
 }
