5e5fcdac09ce5ba2359c0db98b6dc28f7ab8abd7
leesh <qwqw8019@naver.com>
2023-09-17 03:55:26 +0900

    feat: Reservation Request Dto 구현(#35)

---
+++ src/main/java/com/newfit/reservation/dto/request/ReservationRequest.java
@@ -0,0 +1,24 @@
+package com.newfit.reservation.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReservationRequest {
+
+    @NotNull
+    private LocalDateTime startAt;
+
+    @NotNull
+    private LocalDateTime endAt;
+
+    private Long repetitionNumber;
+}
