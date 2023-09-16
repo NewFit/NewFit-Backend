03b1d135445212737516e3beb9b3fdcf95dded07
hoyun06 <hoyun06@gmail.com>
2023-09-16 23:14:23 +0900

    feat: USER6_ROUTINE2 기능 구현을 위해 Request Dto 생성 (#34)

---
+++ src/main/java/com/newfit/reservation/dto/request/UpdateRoutineRequest.java
@@ -0,0 +1,29 @@
+package com.newfit.reservation.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class UpdateRoutineRequest {
+
+    @NotNull
+    private Long routineId;
+
+    @NotNull
+    private Long gymId;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    private List<RoutineEquipmentRequest> routineEquipments;
+}
