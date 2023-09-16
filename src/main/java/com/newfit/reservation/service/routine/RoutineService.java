3458cba9728ac708b233d04981ffbb578d2e4b23
hoyun06 <hoyun06@gmail.com>
2023-09-16 20:59:44 +0900

    feat: USER6_ROUTINE1 기능 구현을 위해 RoutineService.java에 registerRoutine과 validateDuplicate 메소드 작성 (#34)

--- src/main/java/com/newfit/reservation/service/routine/RoutineService.java
+++ src/main/java/com/newfit/reservation/service/routine/RoutineService.java
@@ -1,6 +1,8 @@
 package com.newfit.reservation.service.routine;
 
 
+import com.newfit.reservation.domain.User;
+import com.newfit.reservation.domain.routine.Routine;
 import com.newfit.reservation.repository.routine.RoutineRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,4 +14,23 @@ import org.springframework.transaction.annotation.Transactional;
 public class RoutineService {
 
     private final RoutineRepository routineRepository;
+
+    /*
+    이전에 등록한 루틴과 동일한 이름이라면 exception이 발생합니다.
+    아니라면 새로운 Routine을 등록하고 그 Routine을 반환합니다.
+     */
+    public Routine registerRoutine(User user, String routineName) {
+        if(validateDuplicate(user, routineName))
+            throw new IllegalArgumentException();
+
+        return routineRepository.save(Routine.builder()
+                .user(user)
+                .name(routineName)
+                .build());
+    }
+
+    // 해당 유저가 이전에 등록한 Routine중에 동일한 이름이 있는지 확인합니다.
+    private boolean validateDuplicate(User user, String routineName) {
+        return routineRepository.findByUserAndName(user, routineName).isPresent();
+    }
 }
