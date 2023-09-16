b2130635b7c97fd3842d3eba6a86e5167c6c6156
leesh <qwqw8019@naver.com>
2023-09-17 04:36:03 +0900

    feat: 사용자 id를 통한 예약 조회 (#35)
    ReservationRepository.findAllByReserverId

--- src/main/java/com/newfit/reservation/repository/reservation/ReservationRepository.java
+++ src/main/java/com/newfit/reservation/repository/reservation/ReservationRepository.java
@@ -4,6 +4,10 @@ import com.newfit.reservation.domain.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    List<Reservation> findAllByReserverId(Long reserverId);
 }
