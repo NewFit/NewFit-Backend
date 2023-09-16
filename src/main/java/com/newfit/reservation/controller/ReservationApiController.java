b658fcbf456f49d7977fbafe32ebb95cad0e79b8
leesh <qwqw8019@naver.com>
2023-09-17 04:34:31 +0900

    feat: 예약 조회 기능 controller (#35)

--- src/main/java/com/newfit/reservation/controller/ReservationApiController.java
+++ src/main/java/com/newfit/reservation/controller/ReservationApiController.java
@@ -1,6 +1,7 @@
 package com.newfit.reservation.controller;
 
 import com.newfit.reservation.dto.request.ReservationRequest;
+import com.newfit.reservation.dto.response.ReservationListResponse;
 import com.newfit.reservation.dto.response.ReservationResponse;
 import com.newfit.reservation.service.reservation.ReservationService;
 import jakarta.validation.Valid;
@@ -17,10 +18,15 @@ public class ReservationApiController {
 
 
     @PostMapping
-    public ReservationResponse reserve(
-                                       @RequestParam(value = "equipment_id") Long equipmentId,
+    public ReservationResponse reserve(@RequestParam(value = "equipment_id") Long equipmentId,
                                        @Valid @RequestBody ReservationRequest request) {
-        Long userId = 4L;
+        Long userId = 5L;
         return reservationService.reserve(userId, equipmentId, request);
+    }
+
+    @GetMapping
+    public ReservationListResponse listReservation(@RequestParam(value = "gym_id") Long gymId) {
+        Long userId = 5L;
+        return reservationService.listReservation(userId, gymId);
     }
 }
