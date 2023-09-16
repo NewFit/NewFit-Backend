c0c86d32d4de837399a8dbd02bdb20baeb3786ff
hoyun06 <hoyun06@gmail.com>
2023-09-16 21:00:37 +0900

    feat: USER6_ROUTINE1 기능 구현을 위해 RoutineRepository.java에 findByUserAndName 메소드 작성 (#34)

--- src/main/java/com/newfit/reservation/repository/routine/RoutineRepository.java
+++ src/main/java/com/newfit/reservation/repository/routine/RoutineRepository.java
@@ -1,9 +1,14 @@
 package com.newfit.reservation.repository.routine;
 
+import com.newfit.reservation.domain.User;
 import com.newfit.reservation.domain.routine.Routine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoutineRepository extends JpaRepository<Routine, Long> {
+
+    Optional<Routine> findByUserAndName(User user, String name);
 }
