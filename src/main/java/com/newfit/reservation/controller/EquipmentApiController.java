aaf03a867378b09279b3461bb3a114c6b8fc4f4b
chosw1002@naver.com <chosw1002@naver.com>
2023-09-16 21:16:03 +0900

    feat: "/equipments"로 들어오는 세 가지 API 구현 (#33)

---
+++ src/main/java/com/newfit/reservation/controller/EquipmentApiController.java
@@ -0,0 +1,49 @@
+package com.newfit.reservation.controller;
+
+import com.newfit.reservation.domain.Gym;
+import com.newfit.reservation.domain.equipment.Equipment;
+import com.newfit.reservation.domain.equipment.Purpose;
+import com.newfit.reservation.dto.response.EquipmentGymListResponse;
+import com.newfit.reservation.service.GymService;
+import com.newfit.reservation.service.equipment.EquipmentGymService;
+import com.newfit.reservation.service.equipment.EquipmentService;
+import com.newfit.reservation.service.reservation.ReservationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/equipments")
+@RequiredArgsConstructor
+public class EquipmentApiController {
+    private final EquipmentService equipmentService;
+    private final GymService gymService;
+    private final EquipmentGymService equipmentGymService;
+    private final ReservationService reservationService;
+
+    @GetMapping("")
+    public ResponseEntity<EquipmentGymListResponse> getAllEquipment(@RequestParam(name = "gym_id") Long gymId,
+                                                                    @RequestParam(name = "purpose", required = false) Purpose purpose,
+                                                                    @RequestParam(name = "equipment_id", required = false) Long equipmentId) {
+        Gym gym = gymService.findById(gymId);
+        EquipmentGymListResponse allInGym;
+
+        if (purpose == null && equipmentId == null) {
+            allInGym = equipmentGymService.findAllInGym(gym);
+        } else if (purpose != null && equipmentId == null) {
+            allInGym = equipmentGymService.findAllInGymByPurpose(gym, purpose);
+        } else if (purpose == null && equipmentId != null) {
+            Equipment equipment = equipmentService.findById(equipmentId);
+            allInGym = equipmentGymService.findAllInGymByEquipment(gym, equipment);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(allInGym);
+    }
+}
