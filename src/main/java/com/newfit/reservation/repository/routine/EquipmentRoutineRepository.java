ac3822e8cdfc2aa97a00af7c54908643a2a1b9fa
hoyun06 <hoyun06@gmail.com>
2023-09-17 00:51:31 +0900

    feat: USER6_ROUTINE5 기능 구현을 위해 EquipmentRoutineRepository.java에 findAllByRoutine 메소드 작성 (#34)

--- src/main/java/com/newfit/reservation/repository/routine/EquipmentRoutineRepository.java
+++ src/main/java/com/newfit/reservation/repository/routine/EquipmentRoutineRepository.java
@@ -5,9 +5,14 @@ import com.newfit.reservation.domain.routine.Routine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EquipmentRoutineRepository extends JpaRepository<EquipmentRoutine, Long> {
 
     // 특정 Routine에 묶여있는 모든 EquipmentRoutine 객체를 삭제하는 메소드입니다.
     void deleteAllByRoutine(Routine routine);
+
+    // 특정 Routine에 묶여있는 모든 EquipmentRoutine 객체를 조회하는 메소드입니다.
+    List<EquipmentRoutine> findAllByRoutine(Routine routine);
 }
