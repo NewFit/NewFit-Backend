2b9dc06f0b2569ae3323038f0ddee703546c6fa3
leesh <qwqw8019@naver.com>
2023-09-17 15:00:37 +0900

    feat: 예약 수정 컨트롤러 구현 (#35)

--- src/main/java/com/newfit/reservation/controller/ReservationApiController.java
+++ src/main/java/com/newfit/reservation/controller/ReservationApiController.java
@@ -1,11 +1,13 @@
 package com.newfit.reservation.controller;
 
 import com.newfit.reservation.dto.request.ReservationRequest;
+import com.newfit.reservation.dto.request.ReservationUpdateRequest;
 import com.newfit.reservation.dto.response.ReservationListResponse;
 import com.newfit.reservation.dto.response.ReservationResponse;
 import com.newfit.reservation.service.reservation.ReservationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -18,6 +20,7 @@ public class ReservationApiController {
 
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ReservationResponse reserve(@RequestParam(value = "equipment_id") Long equipmentId,
                                        @Valid @RequestBody ReservationRequest request) {
         Long userId = 5L;
@@ -29,4 +32,12 @@ public class ReservationApiController {
         Long userId = 5L;
         return reservationService.listReservation(userId, gymId);
     }
+
+    @PatchMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ReservationResponse updateReservation(@RequestParam("reservation_id") Long reservationId,
+                                                 @RequestBody ReservationUpdateRequest request){
+        return reservationService.update(reservationId, request);
+    }
+
 }
