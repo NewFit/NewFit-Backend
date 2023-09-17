2739e999d5d5c2123c0e8f89cea0737ccc3a0684
chosw1002@naver.com <chosw1002@naver.com>
2023-09-17 14:44:03 +0900

    chore: Dto 이름에서 Dto 제거 (#39)

--- src/main/java/com/newfit/reservation/service/dev/ReportService.java
+++ src/main/java/com/newfit/reservation/service/dev/ReportService.java
@@ -1,7 +1,7 @@
 package com.newfit.reservation.service.dev;
 
 import com.newfit.reservation.domain.dev.Report;
-import com.newfit.reservation.dto.request.CreateReportRequestDto;
+import com.newfit.reservation.dto.request.CreateReportRequest;
 
 import com.newfit.reservation.repository.dev.ReportRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,10 +18,10 @@ public class ReportService {        // ReportRepository 에 단순 위임하는 
 
     private final ReportRepository reportRepository;
 
-    public Long saveReport(CreateReportRequestDto reportRequestDto) {
+    public Long saveReport(CreateReportRequest reportRequestDto) {
 
         // CreateReportRequestDto 객체를 Report 엔티티 객체로 변환하는 메소드 사용했습니다.
-        Report report = CreateReportRequestDto.reportDto2Entity(reportRequestDto);
+        Report report = CreateReportRequest.reportDto2Entity(reportRequestDto);
         return reportRepository.save(report);
     }
 
