9ca27069402f45901d3651a60f1332cfc2069242
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2023-09-17 16:45:21 +0900

    feat: 기구 조회 API 개발 (#33) (#38)
    * feat: Equipment를 pk로 조회하는 service 메서드 추가 (#33)
    
    * feat: "/equipments"로 들어오는 세 가지 API 구현 (#33)
    
    * chore: EquipmentApiController.java 주석 추가 (#33)
    
    * feat: EquipmentGymList 조회를 위한 service layer 구현 (#33)
    
    * feat: 기구별 상세정보 조회를 위한 response Dto 구현 (#33)
    
    * feat: pk로 EquipmentGym 조회 (#33)
    
    * feat: EquipmentGym로 Reservation 조회 (#33)
    
    * feat: 기구의 예약 불가능 시간 목록을 조회하는 service 구현 (#33)
    
    * feat: Equipment3_INFO1에 해당하는 api 개발 (#33)
    
    * refactor: 최대 예약 시간을 3 -> 2시간으로 수정 (#33)
    
    * refactor: null에 대한 예외 처리 (#33)

--- src/main/java/com/newfit/reservation/service/reservation/ReservationService.java
+++ src/main/java/com/newfit/reservation/service/reservation/ReservationService.java
@@ -1,15 +1,36 @@
 package com.newfit.reservation.service.reservation;
 
 
+import com.newfit.reservation.domain.equipment.EquipmentGym;
+import com.newfit.reservation.domain.reservation.Reservation;
+import com.newfit.reservation.dto.response.EquipmentInfoResponse;
+import com.newfit.reservation.dto.response.OccupiedTime;
 import com.newfit.reservation.repository.reservation.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class ReservationService {
-
     private final ReservationRepository reservationRepository;
+
+    public EquipmentInfoResponse getAllOccupiedTimes(EquipmentGym equipmentGym) {
+        final LocalDateTime now = LocalDateTime.now();
+
+        List<Reservation> reservations = reservationRepository.findAllByEquipmentGym(equipmentGym);
+
+        // reservations에서 현재 시간보다 늦게 끝나거나 현재 시간으로부터 2시간 이후내로 시작되는 예약을 OccupiedTime 리스트로 변환
+        List<OccupiedTime> occupiedTimes = reservations.stream()
+                .filter(reservation -> reservation.getEnd_at().isAfter(now) || reservation.getStart_at().isAfter(now.plusHours(2)))
+                .map(reservation -> new OccupiedTime(reservation.getStart_at(), reservation.getEnd_at()))
+                .collect(Collectors.toList());
+
+        return new EquipmentInfoResponse(equipmentGym, occupiedTimes);
+    }
 }
