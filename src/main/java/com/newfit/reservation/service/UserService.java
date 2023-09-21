feec398cc3b2c247141b8391c98c06e399d903c1
leesh <qwqw8019@naver.com>
2023-09-22 02:35:38 +0900

    feat: 사용자 정보 조회 기능 구현 (#45)

--- src/main/java/com/newfit/reservation/service/UserService.java
+++ src/main/java/com/newfit/reservation/service/UserService.java
@@ -1,12 +1,18 @@
 package com.newfit.reservation.service;
 
+
+import com.newfit.reservation.domain.Authority;
+import com.newfit.reservation.domain.Credit;
 import com.newfit.reservation.domain.User;
-import com.newfit.reservation.dto.request.routine.UserUpdateRequest;
+import com.newfit.reservation.dto.request.UserUpdateRequest;
+import com.newfit.reservation.dto.response.UserDetailResponse;
 import com.newfit.reservation.dto.response.UserSimpleResponse;
 import com.newfit.reservation.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -29,9 +35,41 @@ public class UserService {
                 .build();
     }
 
+    public UserDetailResponse userDetail(Long userId) {
+
+        User user = findOneById(userId);
+
+        Long monthCredit = user.getAuthorityList()
+                .stream()
+                .map(this::getThisMonthCredit)
+                .mapToLong(Long::longValue)
+                .sum();
+
+
+        return UserDetailResponse.builder()
+                .filePath(user.getFilePath())
+                .nickname(user.getNickname())
+                .totalCredit(user.getBalance())
+                .monthCredit(monthCredit)
+                .build();
+    }
+
 
     public User findOneById(Long userId) {
         return userRepository.findById(userId)
                 .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private Long getThisMonthCredit(Authority authority) {
+        LocalDateTime now = LocalDateTime.now();
+        return authority.getCreditList()
+                .stream()
+                .filter(credit ->
+                        credit.getYear().equals((short) now.getYear())
+                                && credit.getMonth().equals((short) now.getMonthValue())
+                )
+                .findAny()
+                .map(Credit::getAmount)
+                .orElse(0L);
     }
 }
