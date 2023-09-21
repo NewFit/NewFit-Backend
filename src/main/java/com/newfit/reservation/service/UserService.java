d449742fec8bc9e3cffbb6a6e9145bccea6d5c39
leesh <qwqw8019@naver.com>
2023-09-22 00:23:01 +0900

    feat: 사용자 정보 수정 기능 구현 (#45)
    - add UserService.modify
    
    - refactor UserService.findOneById by refactoring of UserRepository

--- src/main/java/com/newfit/reservation/service/UserService.java
+++ src/main/java/com/newfit/reservation/service/UserService.java
@@ -1,6 +1,8 @@
 package com.newfit.reservation.service;
 
 import com.newfit.reservation.domain.User;
+import com.newfit.reservation.dto.request.routine.UserUpdateRequest;
+import com.newfit.reservation.dto.response.UserSimpleResponse;
 import com.newfit.reservation.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,8 +15,23 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    public UserSimpleResponse modify(Long userId, UserUpdateRequest request) {
+        User updateUser = findOneById(userId);
+
+        updateUser.updateEmail(request.getEmail());
+        updateUser.updateNickname(request.getNickname());
+        updateUser.updateTel(request.getTel());
+        updateUser.updateFilePath(request.getImage());
+
+
+        return UserSimpleResponse.builder()
+                .userId(userId)
+                .build();
+    }
+
+
     public User findOneById(Long userId) {
-        return userRepository.findOne(userId)
+        return userRepository.findById(userId)
                 .orElseThrow(IllegalArgumentException::new);
     }
 }
