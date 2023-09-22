ccf14ce6c14636ecc1f89c6565c088872b80d0c7
chosw1002@naver.com <chosw1002@naver.com>
2023-09-22 14:40:59 +0900

    feat: nickname으로 회원 조회 메서드 생성 (#52)

--- src/main/java/com/newfit/reservation/repository/UserRepository.java
+++ src/main/java/com/newfit/reservation/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.newfit.reservation.repository;
 import com.newfit.reservation.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByNickname(String nickname);
 }
