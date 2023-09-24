17a5e3fb564d0aad68e5971e35fba9e072c35cb4
chosw1002@naver.com <chosw1002@naver.com>
2023-09-24 21:56:14 +0900

    feat: 등록된 Gym이 없는 회원의 TokenAuthenticationFilter 로직 추가 (#65)

--- src/main/java/com/newfit/reservation/common/config/TokenAuthenticationFilter.java
+++ src/main/java/com/newfit/reservation/common/config/TokenAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -22,8 +23,11 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String accessToken = getAccessToken(request);
 
-        if(tokenProvider.validToken(accessToken, request)) {
+        if(requiresValidityCheck(request) && tokenProvider.validToken(accessToken, request)) {
             Authentication authentication = tokenProvider.getAuthentication(accessToken, request);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else {
+            Authentication authentication = tokenProvider.getAnonymousAuthentication(accessToken);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
         filterChain.doFilter(request, response);
@@ -35,5 +39,12 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
             return authorizationHeader.substring(BEARER.length());
         }
         return null;
+    }
+
+    private boolean requiresValidityCheck(HttpServletRequest request) {
+        if (request.getRequestURI().equals("/api/v1/authority") && request.getMethod().equals(HttpMethod.POST.toString())) {
+            return false;
+        }
+        return true;
     }
 }
