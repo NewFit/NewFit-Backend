a65a0c1ccf0ac739dcfc25de386bad17319d07f9
chosw1002@naver.com <chosw1002@naver.com>
2023-09-24 14:54:47 +0900

    feat: OAuth2와 Jwt 적용을 위한 SecurityConfig 구현 (#52)

--- src/main/java/com/newfit/reservation/common/config/WebSecurityConfig.java
+++ src/main/java/com/newfit/reservation/common/config/WebSecurityConfig.java
@@ -1,49 +1,77 @@
 package com.newfit.reservation.common.config;
 
-import com.newfit.reservation.common.auth.UserDetailService;
+import com.newfit.reservation.common.jwt.TokenProvider;
+import com.newfit.reservation.common.oauth.OAuth2AuthorizationRequestCookieRepository;
+import com.newfit.reservation.common.oauth.handler.OAuth2FailureHandler;
+import com.newfit.reservation.common.oauth.handler.OAuth2SuccessHandler;
+import com.newfit.reservation.common.oauth.OAuth2UserCustomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.stream.Stream;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
+@EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
 public class WebSecurityConfig {
-    private final UserDetailService userDetailService;
+    private final TokenProvider tokenProvider;
+    private final OAuth2UserCustomService oAuth2UserCustomService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final OAuth2FailureHandler oAuth2FailureHandler;
+
+    private static final String[] PERMIT_ALL_PATTERNS = new String[] {
+            "/login/**"
+    };
 
     @Bean
     public WebSecurityCustomizer configure() {
         return (web) -> web.ignoring()
                 .requestMatchers(toH2Console())
-                .requestMatchers("/static/**");
+                .requestMatchers(AntPathRequestMatcher.antMatcher("/static/**"));
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(authorizeHttpRequests ->
-                        authorizeHttpRequests
-                                .requestMatchers("/login", "signup").permitAll()
-                                .anyRequest().authenticated())
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login"))
                 .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                    .requestMatchers(Stream.of(PERMIT_ALL_PATTERNS).map(AntPathRequestMatcher::antMatcher).toArray(AntPathRequestMatcher[]::new)).permitAll()
+                    .requestMatchers(AntPathRequestMatcher.antMatcher("/api/**")).authenticated()
+                    .anyRequest().permitAll())
+                .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .oauth2Login(oauth -> oauth
+                    .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(oAuth2UserCustomService))
+                    .successHandler(oAuth2SuccessHandler)
+                    .failureHandler(oAuth2FailureHandler)
+                    .authorizationEndpoint(authorizationEndpoint -> authorizationEndpoint
+                        .authorizationRequestRepository(oAuth2AuthorizationRequestCookieRepository())))
+                .logout(logout -> logout
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/login"))
                 .build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, UserDetailService userDetailService) throws Exception {
-        // TODO: and is deprecated.
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userDetailService)
-                .and().build();
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
+    }
+
+    @Bean
+    public OAuth2AuthorizationRequestCookieRepository oAuth2AuthorizationRequestCookieRepository() {
+        return new OAuth2AuthorizationRequestCookieRepository();
     }
 }
