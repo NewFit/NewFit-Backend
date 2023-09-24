c417fe4b9141489b2871ae339e1cf6534cb86094
chosw1002@naver.com <chosw1002@naver.com>
2023-09-24 14:59:25 +0900

    feat: Provider에 description 추가 (#52)

--- src/main/java/com/newfit/reservation/domain/Provider.java
+++ src/main/java/com/newfit/reservation/domain/Provider.java
@@ -1,5 +1,18 @@
 package com.newfit.reservation.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Provider {
-    GOOGLE, KAKAO
+    GOOGLE("google"), KAKAO("kakao");
+
+    private final String description;
+    public static Provider getProvider(String description) {
+        if (description.equals(Provider.GOOGLE.description)) {
+            return Provider.GOOGLE;
+        }
+        return Provider.KAKAO;
+    }
 }
