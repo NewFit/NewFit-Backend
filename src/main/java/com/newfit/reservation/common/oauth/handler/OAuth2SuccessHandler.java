5433397d75bac29ddbed74eb268088c8f7a2c037
hoyun06 <hoyun06@gmail.com>
2023-09-25 16:38:35 +0900

    chore: OAuth2SuccessHandler 주석 추가 (#61)

--- src/main/java/com/newfit/reservation/common/oauth/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/newfit/reservation/common/oauth/handler/OAuth2SuccessHandler.java
@@ -31,7 +31,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
         OAuthHistory oAuthHistory = oAuth2User.getOAuthHistory();
-        if (oAuthHistory.isRegistered()) {
+        if (oAuthHistory.isRegistered()) {  // 회원가입이 완료된 경우
             Authority authority = oAuthHistory.getUser().getAuthorityList().stream().findAny().orElse(null);
             String accessToken = tokenProvider.generateToken(oAuthHistory.getUser(), ACCESS_TOKEN_DURATION);
             String refreshToken = tokenProvider.generateToken(oAuthHistory.getUser(), REFRESH_TOKEN_DURATION);
@@ -39,15 +39,15 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
             response.setHeader("accessToken", accessToken);
             response.setHeader("refreshToken", refreshToken);
-            if (authority != null) {
+            if (authority != null) {    // 등록된 gym이 있는 경우 자신의 헬스장 기구 전체 조회 api 호출
                 response.setHeader("authorityId", authority.getId().toString());
                 String redirectUri = getTargetUri(authority.getGym().getId());
                 response.sendRedirect(redirectUri);
             }
-            else {
+            else {  // 등록된 gym이 없는 경우 헬스장 전체 조회 api 호출
                 response.sendRedirect("/api/v1/gyms");
             }
-        } else {
+        } else {    // 회원가입이 미실시된 경우
             String accessToken = tokenProvider.generateToken(oAuthHistory.getUser(), ACCESS_TOKEN_DURATION);
             response.setHeader("accessToken", accessToken);
             // TODO: 회원 가입 추가 정보 입력 페이지로 이동
