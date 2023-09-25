ea8d35441f7122a22306a9e4ca80ca69edcae52b
leesh <qwqw8019@naver.com>
2023-09-26 01:50:30 +0900

    fix: @Repository 어노테이션 제거 (#50)

--- src/main/java/com/newfit/reservation/repository/equipment/EquipmentGymRepository.java
+++ src/main/java/com/newfit/reservation/repository/equipment/EquipmentGymRepository.java
@@ -2,16 +2,13 @@ package com.newfit.reservation.repository.equipment;
 
 
 import com.newfit.reservation.domain.Gym;
-import com.newfit.reservation.domain.equipment.Equipment;
 import com.newfit.reservation.domain.equipment.EquipmentGym;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface EquipmentGymRepository extends JpaRepository<EquipmentGym, Long> {
     List<EquipmentGym> findAllByGym(Gym gym);
 
