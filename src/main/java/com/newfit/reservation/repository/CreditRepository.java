c6954a5dd2b788866a993605eca85afc63a8428a
chosw1002@naver.com <chosw1002@naver.com>
2023-09-25 17:59:42 +0900

    feat: Authority, year, month로 credit 조회 (#56)

--- src/main/java/com/newfit/reservation/repository/CreditRepository.java
+++ src/main/java/com/newfit/reservation/repository/CreditRepository.java
@@ -6,15 +6,15 @@ import com.newfit.reservation.domain.Gym;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
+import java.util.Optional;
 
 public interface CreditRepository extends JpaRepository<Credit, Long> {
 
-    List<Credit> findAllByAuthorityAndYearAndMonth(Authority authority,
-                                                 Short year,
-                                                 Short month);
+    List<Credit> findAllByAuthorityAndYearAndMonth(Authority authority, Short year, Short month);
 
     @Query("select c from Credit c where c.authority.gym =:gym and c.year =:year and c.month =:month order by c.amount desc ")
     List<Credit> findAllByGymAndYearAndMonth(@Param("gym") Gym gym, @Param("year") Short year, @Param("month") Short month);
+
+    Optional<Credit> findByAuthorityAndYearAndMonth(Authority authority, Short year, Short month);
 }
