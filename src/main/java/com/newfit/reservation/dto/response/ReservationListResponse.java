eaaa9749f20c09efd7c72516fdfd3b674ef9d983
Lee-sh98 <55966515+Lee-sh98@users.noreply.github.com>
2023-09-30 16:15:07 +0900

    feat: snake case 적용 (#75) (#78)

--- src/main/java/com/newfit/reservation/dto/response/ReservationListResponse.java
+++ src/main/java/com/newfit/reservation/dto/response/ReservationListResponse.java
@@ -1,6 +1,8 @@
 package com.newfit.reservation.dto.response;
 
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +12,7 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ReservationListResponse {
 
     private String gymName;
