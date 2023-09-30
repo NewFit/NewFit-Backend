f85d4832460199961ecdd2a6847187bb991076cb
chosw1002@naver.com <chosw1002@naver.com>
2023-09-30 16:31:30 +0900

    feat: 시작시간으로 예약 조회 (#58)

--- src/main/java/com/newfit/reservation/repository/reservation/ReservationRepository.java
+++ src/main/java/com/newfit/reservation/repository/reservation/ReservationRepository.java
@@ -5,15 +5,16 @@ import com.newfit.reservation.domain.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
+import java.time.LocalDateTime;
 import java.util.List;
 
-@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     @Query("select r from Reservation r where r.reserver.id = :authorityId")
     List<Reservation> findAllByAuthorityId(@Param("authorityId") Long authorityId);
 
     List<Reservation> findAllByEquipmentGym(EquipmentGym equipmentGym);
+
+    @Query("select r from Reservation r where r.start_at =:startAt")
+    List<Reservation> findAllByStart_at(@Param("startAt") LocalDateTime startAt);
 }
