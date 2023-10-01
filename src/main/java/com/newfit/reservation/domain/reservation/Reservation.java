52f17da981e9915784e73ab3c5f31ce55432ef13
chosw1002@naver.com <chosw1002@naver.com>
2023-10-01 16:23:41 +0900

    feat: 예약의 태그 시간 update 메서드 구현 (#70)

--- src/main/java/com/newfit/reservation/domain/reservation/Reservation.java
+++ src/main/java/com/newfit/reservation/domain/reservation/Reservation.java
@@ -4,13 +4,11 @@ import com.newfit.reservation.domain.Authority;
 import com.newfit.reservation.domain.common.BaseTimeEntity;
 import com.newfit.reservation.domain.equipment.EquipmentGym;
 import com.newfit.reservation.dto.request.ReservationRequest;
-import com.newfit.reservation.dto.request.ReservationUpdateRequest;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
 @Getter
@@ -77,6 +75,10 @@ public class Reservation extends BaseTimeEntity {
 
     public void updateRepetitionNumber(Long repetitionNumber) {
         this.repetition_number = repetitionNumber;
+    }
+
+    public void updateStartTagAt(LocalDateTime startTagAt) {
+        this.startTagAt = startTagAt;
     }
 
     public boolean overlapped(LocalDateTime start, LocalDateTime end) {
