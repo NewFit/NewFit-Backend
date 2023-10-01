ee6587f6c70469a844f04baad9c53ae15d2d123e
hoyun06 <hoyun06@gmail.com>
2023-10-01 13:11:48 +0900

    feat: CREDIT1_OBTAIN1을 위한 Dto 클래스 생성 (#79)

---
+++ src/main/java/com/newfit/reservation/dto/response/ObtainCreditResponse.java
@@ -0,0 +1,16 @@
+package com.newfit.reservation.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ObtainCreditResponse {
+
+    private final Long balance;
+    private final Long increasedAmount;
+    private final String message;
+}
