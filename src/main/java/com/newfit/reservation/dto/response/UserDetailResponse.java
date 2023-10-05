57cdcaf20247a6f971fa97b19b9f365f1a3802f5
hoyun06 <hoyun06@gmail.com>
2023-10-05 16:26:52 +0900

    refactor: Dto에 필드 및 생성 메소드 추가 (#102)

--- src/main/java/com/newfit/reservation/dto/response/UserDetailResponse.java
+++ src/main/java/com/newfit/reservation/dto/response/UserDetailResponse.java
@@ -2,7 +2,11 @@ package com.newfit.reservation.dto.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.newfit.reservation.domain.Authority;
+import com.newfit.reservation.domain.User;
 import lombok.*;
+
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -10,21 +14,43 @@ import lombok.*;
 public class UserDetailResponse {
 
     private String profileFilePath;
-
     private String nickname;
-
     private Long totalCredit;
-
     private Long thisMonthCredit;
+    private Long currentAuthorityId;
+    private String currentGymName;
+    private Integer authorityGymsCount;
+    private List<AuthorityGymResponse> authorityGyms;
 
     @Builder
     private UserDetailResponse(String filePath,
                                String nickname,
                                Long totalCredit,
-                               Long monthCredit) {
+                               Long monthCredit,
+                               Long currentAuthorityId,
+                               String currentGymName,
+                               List<AuthorityGymResponse> authorityGyms) {
         this.profileFilePath = filePath;
         this.nickname = nickname;
         this.totalCredit = totalCredit;
         this.thisMonthCredit = monthCredit;
+        this.currentAuthorityId = currentAuthorityId;
+        this.currentGymName = currentGymName;
+        this.authorityGyms = authorityGyms;
+        this.authorityGymsCount = authorityGyms.size();
+    }
+
+    public static UserDetailResponse createUserDetailResponse(User user, Long monthCredit, Authority authority
+            , List<AuthorityGymResponse> authorityGyms) {
+
+        return UserDetailResponse.builder()
+                .filePath(user.getFilePath())
+                .nickname(user.getNickname())
+                .totalCredit(user.getBalance())
+                .monthCredit(monthCredit)
+                .currentAuthorityId(authority.getId())
+                .currentGymName(authority.getGym().getName())
+                .authorityGyms(authorityGyms)
+                .build();
     }
 }
