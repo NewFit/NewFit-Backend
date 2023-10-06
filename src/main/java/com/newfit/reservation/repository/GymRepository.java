6064e73a37ecb3311d34b528bf6c9d86dfa4d94d
chosw1002@naver.com <chosw1002@naver.com>
2023-10-06 15:31:53 +0900

    chore: 불필요한 어노테이션 제거 (#126)

--- src/main/java/com/newfit/reservation/repository/GymRepository.java
+++ src/main/java/com/newfit/reservation/repository/GymRepository.java
@@ -2,8 +2,6 @@ package com.newfit.reservation.repository;
 
 import com.newfit.reservation.domain.Gym;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface GymRepository extends JpaRepository<Gym, Long> {
 }
