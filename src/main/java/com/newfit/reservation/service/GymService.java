e9e456c45e16c8e606df088c0d4d70d0bcdec023
hoyun06 <hoyun06@gmail.com>
2023-10-06 23:41:05 +0900

    refactor: 검색 기능 추가에 따른 수정 사항 (#133)
    - GymRepository, GymService

--- src/main/java/com/newfit/reservation/service/GymService.java
+++ src/main/java/com/newfit/reservation/service/GymService.java
@@ -7,30 +7,27 @@ import com.newfit.reservation.repository.GymRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class GymService {
     private final GymRepository gymRepository;
 
-    public Long save(Gym gym) {
-        return gymRepository.save(gym).getId();
-    }
-
-    /*
-    GymRepository로부터 NewFit 서비스에 등록된 모든 헬스장을 list 형태로 받습니다.
-    그 다음에 Dto로 변환하여 Controller에게 반환합니다.
-     */
-    public GymListResponse getAllGyms() {
-        List<Gym> findGyms = gymRepository.findAll();
-
+    public GymListResponse searchGyms(String gymName) {
+        String keywordString = processQueryParam(gymName);
+        List<Gym> findGyms = gymRepository.findAllByNameContaining(keywordString);
         List<GymResponse> gyms = findGyms.stream()
                 .map(GymResponse::new)
-                .collect(Collectors.toList());
+                .toList();
 
-        return GymListResponse.builder()
-                .gyms(gyms)
-                .build();
+        return GymListResponse.createGymListResponse(gyms);
+    }
+
+    private String processQueryParam(String gymName) {
+        if (gymName == null) {
+            return "'()'";
+        }
+        String processedGymName = gymName.replace("헬스장", "").trim().replaceAll("\s+", "|");
+        return "'(" + processedGymName + ")'";
     }
 }
