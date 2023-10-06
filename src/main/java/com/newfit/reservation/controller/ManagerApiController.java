03b75c266318455d544d5764d547118396b6b21f
chosw1002@naver.com <chosw1002@naver.com>
2023-10-06 09:42:16 +0900

    rename: staff를 manager로 변경 (#118)

--- src/main/java/com/newfit/reservation/controller/ManagerApiController.java
+++ src/main/java/com/newfit/reservation/controller/ManagerApiController.java
@@ -18,9 +18,9 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/staff")
+@RequestMapping("/api/v1/manager")
 @RequiredArgsConstructor
-public class StaffApiController {
+public class ManagerApiController {
     private final EquipmentService equipmentService;
     private final AuthorityService authorityService;
     private final EquipmentGymService equipmentGymService;
