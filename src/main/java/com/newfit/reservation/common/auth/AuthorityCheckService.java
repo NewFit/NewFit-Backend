57384c1557c05fb6523cf4333cbe2294114a8160
leesh <qwqw8019@naver.com>
2023-10-09 01:52:07 +0900

    refactor: AuthorityCheckService custom exception 적용 (#158)

--- src/main/java/com/newfit/reservation/common/auth/AuthorityCheckService.java
+++ src/main/java/com/newfit/reservation/common/auth/AuthorityCheckService.java
@@ -1,12 +1,15 @@
 package com.newfit.reservation.common.auth;
 
 import com.newfit.reservation.domain.Authority;
+import com.newfit.reservation.exception.CustomException;
 import com.newfit.reservation.repository.AuthorityRepository;
 import com.newfit.reservation.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
+
+import static com.newfit.reservation.exception.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -18,22 +21,22 @@ public class AuthorityCheckService {
     public void validateByUserId(Authentication authentication, Long userId) {
         User principal = (User) authentication.getPrincipal();
         com.newfit.reservation.domain.User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("user를 찾을 수 없습니다."));
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
 
         String nickname = principal.getUsername();
         if (!user.getNickname().equals(nickname)) {
-            throw new IllegalArgumentException("권한이 일치하지 않습니다.");
+            throw new CustomException(UNAUTHORIZED_REQUEST);
         }
     }
 
     public void validateByAuthorityId(Authentication authentication, Long authorityId) {
         User principal = (User) authentication.getPrincipal();
         Authority authority = authorityRepository.findById(authorityId)
-                .orElseThrow(() -> new IllegalArgumentException("authority를 찾을 수 없습니다."));
+                .orElseThrow(() -> new CustomException(AUTHORITY_NOT_FOUND));
 
         String nickname = principal.getUsername();
         if (!authority.getUser().getNickname().equals(nickname)) {
-            throw new IllegalArgumentException("권한이 일치하지 않습니다.");
+            throw new CustomException(UNAUTHORIZED_REQUEST);
         }
     }
 }
