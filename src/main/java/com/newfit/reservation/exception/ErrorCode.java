fcb6f9171b4e7526350f05374ee1d83aed72a13d
leesh <qwqw8019@naver.com>
2023-10-08 14:39:57 +0900

    feat: ErrorCode 생성 (#107)

---
+++ src/main/java/com/newfit/reservation/exception/ErrorCode.java
@@ -0,0 +1,33 @@
+package com.newfit.reservation.exception;
+
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+
+
+    // 404 NOT_FOUND 잘못된 리소스 접근
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
+    GYM_NOT_FOUND(HttpStatus.NOT_FOUND, "헬스장을 찾을 수 없습니다."),
+    AUTHORITY_NOT_FOUND(HttpStatus.NOT_FOUND, "권한을 찾을 수 없습니다."),
+    EQUIPMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "기구 종류를 찾을 수 없습니다."),
+    EQUIPMENT_GYM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 기구를 찾을 수 없습니다."),
+    EQUIPMENT_ROUTINE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 루틴 기구를 찾을 수 없습니다."),
+    ROUTINE_NOT_FOUND(HttpStatus.NOT_FOUND, "루틴을 찾을 수 없습니다."),
+
+    // 500 INTERNAL SERVER ERROR
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다.")
+    ;
+
+    private final String name;
+    private final int statusCode;
+    private final String message;
+
+    ErrorCode(HttpStatus status, String message){
+        this.name = status.name();
+        this.statusCode = status.value();
+        this.message = message;
+    }
+}
