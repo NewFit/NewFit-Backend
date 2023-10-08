796a04d35b0917a9644d43a78a83b869806f62db
leesh <qwqw8019@naver.com>
2023-10-08 14:41:05 +0900

    feat: GlobalExceptionHandler 생성 (#107)

---
+++ src/main/java/com/newfit/reservation/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.newfit.reservation.exception.handler;
+
+
+import com.newfit.reservation.exception.CustomException;
+import com.newfit.reservation.exception.ExceptionResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<ExceptionResponse> customException(CustomException exception){
+        return ResponseEntity
+                .status(exception.getErrorCode().getStatusCode())
+                .body(ExceptionResponse.create(exception));
+    }
+
+
+}
