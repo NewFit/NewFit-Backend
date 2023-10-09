fdf8b77c3b702b83ec00e2630bf055a67e9dc60a
leesh <qwqw8019@naver.com>
2023-10-09 19:58:37 +0900

    feat: access level 적용, allArgsConstructor 삭제 (#157)

--- src/main/java/com/newfit/reservation/dto/response/CreditRanking.java
+++ src/main/java/com/newfit/reservation/dto/response/CreditRanking.java
@@ -2,15 +2,14 @@ package com.newfit.reservation.dto.response;
 
 import com.newfit.reservation.domain.Credit;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class CreditRanking extends Credit{
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CreditRanking extends Credit {
 
     @Column(nullable = false)
     private Long rank;
