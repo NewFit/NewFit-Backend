225601befea4dc9bf395d6c7a8cb240cf7924de7
hoyun06 <97820785+hoyun06@users.noreply.github.com>
2023-10-09 11:04:20 +0900

    chore: 불필요한 어노테이션 제거 (#141) (#160)
    * chore: 불필요한 어노테이션 제거 (#141)
    
    * chore: Credit 내부 불필요한 어노테이션 제거 (#141)
    
    * feat: Credit에 Builder 추가 (#141)

--- src/main/java/com/newfit/reservation/domain/Credit.java
+++ src/main/java/com/newfit/reservation/domain/Credit.java
@@ -9,8 +9,6 @@ import java.time.LocalDate;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)  // Entity 클래스는 기본생성자가 필수로 있어야 합니다.
                                                     // 다만, Entity 객체를 직접 생성자로 생성할 일은 없을듯 하여 protected 로 설정했습니다.
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
 public class Credit {
 
     // Credit 테이블 PK 입니다.
@@ -49,18 +47,26 @@ public class Credit {
     // Credit 생성 메소드에서 초기 amount 값 설정하기 위한 필드입니다.
     // @Transient 어노테이션을 사용하여 영속성 컨텍스트의 관리를 받지 않도록 했습니다.
     @Transient
-    private static final Long INITIAL_VALUE = 0L;
+    private final Long INITIAL_VALUE = 0L;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Credit(Authority authority, short year, short month) {
+        this.authority = authority;
+        this.year = year;
+        this.month = month;
+        this.amount = INITIAL_VALUE;
+    }
 
     // 생성 메소드입니다
     /* getMonth 메소드가 아니라 getMonthValue 메소드 사용한 이유
      -> ENUM 값이 아닌 1 ~ 12의 int 값을 바로 리턴해 주기 때문입니다. */
-    public static Credit createCredit() {
+    public static Credit createCredit(Authority authority) {
         LocalDate now = LocalDate.now();
 
         return Credit.builder()
+                .authority(authority)
                 .year((short) now.getYear())
                 .month((short) now.getMonthValue())
-                .amount(INITIAL_VALUE)
                 .build();
     }
 
