c69fe62055439e51dc69d52dd897533b11454235
leesh <qwqw8019@naver.com>
2023-10-09 16:40:05 +0900

    refactor: 순위 조회 기능 수정 (#157)
    - dense_rank() 사용에 따른 수정
    - 사용자 순위 조회 기능 추가

--- src/main/java/com/newfit/reservation/service/CreditService.java
+++ src/main/java/com/newfit/reservation/service/CreditService.java
@@ -1,15 +1,13 @@
 package com.newfit.reservation.service;
 
 import com.newfit.reservation.domain.Authority;
-import com.newfit.reservation.domain.Credit;
+import com.newfit.reservation.dto.response.CreditRanking;
 import com.newfit.reservation.dto.response.UserRankInfo;
 import com.newfit.reservation.dto.response.UserRankInfoListResponse;
 import com.newfit.reservation.exception.CustomException;
 import com.newfit.reservation.repository.AuthorityRepository;
 import com.newfit.reservation.repository.CreditRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
@@ -32,23 +30,19 @@ public class CreditService {
                 .orElseThrow(() -> new CustomException(AUTHORITY_NOT_FOUND));
         List<UserRankInfo> rankingList = new ArrayList<>();
 
-        Pageable topTen = PageRequest.of(0, 10);
-        List<Credit> creditList = creditRepository.findAllByGymAndYearAndMonth(authority.getGym(), (short) now.getYear(), (short) now.getMonthValue(), topTen).stream().toList();
-        creditList.forEach((credit -> rankingList.add(new UserRankInfo(credit, getRank(rankingList, credit)))));
-        return new UserRankInfoListResponse(authority.getGym().getName(), rankingList);
-    }
+        List<CreditRanking> creditList = creditRepository
+                .findAllByGymIdAndYearAndMonth(authority.getGym().getId(), (short) now.getYear(), (short) now.getMonthValue());
 
-    private Long getRank(List<UserRankInfo> rankingList, Credit credit) {
-        if (rankingList.isEmpty()) {
-            return 1L;
-        }
+        rankingList = creditList.subList(0, 10)
+                .stream()
+                .map(UserRankInfo::new)
+                .toList();
 
-        // credit 동점자 순위 처리
-        int indexOfLastElement = rankingList.size() - 1;
-        if (rankingList.get(indexOfLastElement).getAmount().equals(credit.getAmount())) {
-            return rankingList.get(indexOfLastElement).getRank();
-        } else {
-            return rankingList.get(indexOfLastElement).getRank() + 1L;
-        }
+        UserRankInfo userRankInfo = creditList.stream()
+                .filter(c -> c.getAuthority().equals(authority))
+                .map(UserRankInfo::new)
+                .findAny().orElse(null);
+
+        return new UserRankInfoListResponse(authority.getGym().getName(), rankingList, userRankInfo);
     }
 }
