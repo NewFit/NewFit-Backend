8d1de9fa15e806cdc38506f759605b12db967bc8
hoyun06 <hoyun06@gmail.com>
2023-10-10 21:11:51 +0900

    feat: AuthorityApiController에 AccessToken 발급 로직 추가 (#172)

--- src/main/java/com/newfit/reservation/controller/AuthorityApiController.java
+++ src/main/java/com/newfit/reservation/controller/AuthorityApiController.java
@@ -1,32 +1,42 @@
 package com.newfit.reservation.controller;
 
 import com.newfit.reservation.common.auth.AuthorityCheckService;
+import com.newfit.reservation.common.auth.jwt.TokenProvider;
+import com.newfit.reservation.domain.User;
 import com.newfit.reservation.dto.request.AuthorityRequest;
 import com.newfit.reservation.dto.request.EntryRequest;
 import com.newfit.reservation.dto.response.GymListResponse;
 import com.newfit.reservation.dto.response.ReservationListResponse;
 import com.newfit.reservation.service.AuthorityService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import static org.springframework.http.HttpStatus.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.HttpStatus.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/authority")
 @RequiredArgsConstructor
 public class AuthorityApiController {
     private final AuthorityService authorityService;
     private final AuthorityCheckService authorityCheckService;
+    private final TokenProvider tokenProvider;
 
     @PostMapping
     public ResponseEntity<Void> register(Authentication authentication,
                                          @RequestHeader(value = "user-id") Long userId,
-                                         @Valid @RequestBody AuthorityRequest request) {
+                                         @Valid @RequestBody AuthorityRequest request,
+                                         HttpServletResponse response) {
         authorityCheckService.validateByUserId(authentication, userId);
-        authorityService.register(userId, request.getGymId());
+        User user = authorityService.register(userId, request.getGymId());
+        String accessToken = tokenProvider.generateAccessToken(user);
+        log.info("AuthorityRegister.accessToken = {}", accessToken);
+        response.setHeader("access-token", accessToken);
 
         return ResponseEntity
                 .status(CREATED)
@@ -42,9 +52,13 @@ public class AuthorityApiController {
 
     @DeleteMapping
     public ResponseEntity<Void> delete(Authentication authentication,
-                                       @RequestHeader(value = "authority-id") Long authorityId) {
+                                       @RequestHeader(value = "authority-id") Long authorityId,
+                                       HttpServletResponse response) {
         authorityCheckService.validateByAuthorityId(authentication, authorityId);
-        authorityService.delete(authorityId);
+        User user = authorityService.delete(authorityId);
+        String accessToken = tokenProvider.generateAccessToken(user);
+        log.info("AuthorityDelete.accessToken = {}", accessToken);
+        response.setHeader("access-token", accessToken);
 
         return ResponseEntity
                 .noContent()
