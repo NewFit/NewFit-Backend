65c618e5459fe828013d7caa2361b46c16dd82e5
hoyun06 <hoyun06@gmail.com>
2023-10-12 16:23:14 +0900

    feat: AuthorityCheckService 내부에 현재 헤더에 담긴 값을 구분하여 validation 로직을 실행하는 메소드 추가 (#176)

--- src/main/java/com/newfit/reservation/common/auth/AuthorityCheckService.java
+++ src/main/java/com/newfit/reservation/common/auth/AuthorityCheckService.java
@@ -4,6 +4,7 @@ import com.newfit.reservation.domain.Authority;
 import com.newfit.reservation.exception.CustomException;
 import com.newfit.reservation.repository.AuthorityRepository;
 import com.newfit.reservation.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
@@ -18,13 +19,12 @@ public class AuthorityCheckService {
     private final UserRepository userRepository;
     private final AuthorityRepository authorityRepository;
 
-    public void validateByUserId(Authentication authentication, Long userId) {
-        User principal = (User) authentication.getPrincipal();
-        com.newfit.reservation.domain.User user = userRepository.findById(userId)
-                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
-
-        String nickname = principal.getUsername();
-        if (!user.getNickname().equals(nickname)) {
+    public void checkHeaderAndValidateAuthority(Authentication authentication, HttpServletRequest request) {
+        if (request.getHeader("user-id") != null) {
+            validateByUserId(authentication, Long.parseLong(request.getHeader("user-id")));
+        } else if (request.getHeader("authority-id") != null){
+            validateByAuthorityId(authentication,Long.parseLong(request.getHeader("authority-id")));
+        } else {
             throw new CustomException(UNAUTHORIZED_REQUEST);
         }
     }
@@ -36,6 +36,17 @@ public class AuthorityCheckService {
 
         String nickname = principal.getUsername();
         if (!authority.getUser().getNickname().equals(nickname)) {
+            throw new CustomException(UNAUTHORIZED_REQUEST);
+        }
+    }
+
+    private void validateByUserId(Authentication authentication, Long userId) {
+        User principal = (User) authentication.getPrincipal();
+        com.newfit.reservation.domain.User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
+        String nickname = principal.getUsername();
+        if (!user.getNickname().equals(nickname)) {
             throw new CustomException(UNAUTHORIZED_REQUEST);
         }
     }
