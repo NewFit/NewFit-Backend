911d0eaa80557a617f859039960fded27393c0d9
hoyun06 <hoyun06@gmail.com>
2023-10-14 10:32:42 +0900

    feat: ProposalRepository, ReportRepository에 findAllByUser 메소드 추가 (#178)

--- src/main/java/com/newfit/reservation/repository/dev/ProposalRepository.java
+++ src/main/java/com/newfit/reservation/repository/dev/ProposalRepository.java
@@ -1,7 +1,11 @@
 package com.newfit.reservation.repository.dev;
 
+import com.newfit.reservation.domain.User;
 import com.newfit.reservation.domain.dev.Proposal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProposalRepository extends JpaRepository<Proposal, Long> {
+    List<Proposal> findAllByUser(User user);
 }
--- src/main/java/com/newfit/reservation/repository/dev/ReportRepository.java
+++ src/main/java/com/newfit/reservation/repository/dev/ReportRepository.java
@@ -1,7 +1,11 @@
 package com.newfit.reservation.repository.dev;
 
+import com.newfit.reservation.domain.User;
 import com.newfit.reservation.domain.dev.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReportRepository extends JpaRepository<Report, Long> {
+    List<Report> findAllByUser(User user);
 }
