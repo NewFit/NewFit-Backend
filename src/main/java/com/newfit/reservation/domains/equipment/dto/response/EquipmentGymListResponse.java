e13295fb63e19ee7642237c4b96172bd8f624c50
chosw1002@naver.com <chosw1002@naver.com>
2023-10-18 10:19:12 +0900

    refactor: query의 개선을 통해 service의 로직을 단순화합니다. (#202)
    - repository 메서드 추가
    - DTO의 생성 방식 변경

--- src/main/java/com/newfit/reservation/domains/equipment/dto/response/EquipmentGymListResponse.java
+++ src/main/java/com/newfit/reservation/domains/equipment/dto/response/EquipmentGymListResponse.java
@@ -2,17 +2,31 @@ package com.newfit.reservation.domains.equipment.dto.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Getter
-@RequiredArgsConstructor
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class EquipmentGymListResponse {
     private final String gymName;
     private final Integer equipmentsCount;
     private final List<EquipmentResponse> equipments;
 
+    @Builder(access = AccessLevel.PRIVATE)
+    private EquipmentGymListResponse(String gymName, List<EquipmentResponse> equipments) {
+        this.gymName = gymName;
+        this.equipmentsCount = equipments.size();
+        this.equipments = equipments;
+    }
+
+    public static EquipmentGymListResponse createResponse(String gymName, List<EquipmentResponse> equipments) {
+        return EquipmentGymListResponse
+                .builder()
+                .gymName(gymName)
+                .equipments(equipments)
+                .build();
+    }
 }
