60cc366839a09ac122fb75dddb28e424b5362229
hoyun06 <hoyun06@gmail.com>
2023-10-26 15:19:56 +0900

    feat: CustomExceptionHandlingFilter를 bean으로 등록하는 부분 추가 및 addFilterBefore 메소드 추가 (#245)

--- src/main/java/com/newfit/reservation/common/auth/config/WebSecurityConfig.java
+++ src/main/java/com/newfit/reservation/common/auth/config/WebSecurityConfig.java
@@ -1,13 +1,14 @@
 package com.newfit.reservation.common.auth.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.newfit.reservation.common.auth.jwt.TokenAuthenticationFilter;
 import com.newfit.reservation.common.auth.jwt.TokenProvider;
 import com.newfit.reservation.common.auth.oauth.OAuth2AuthorizationRequestCookieRepository;
 import com.newfit.reservation.common.auth.oauth.OAuth2UserCustomService;
 import com.newfit.reservation.common.auth.oauth.handler.OAuth2FailureHandler;
 import com.newfit.reservation.common.auth.oauth.handler.OAuth2SuccessHandler;
+import com.newfit.reservation.common.exception.CustomExceptionHandlingFilter;
 import com.newfit.reservation.domains.auth.repository.RefreshTokenRepository;
-import com.newfit.reservation.domains.authority.repository.AuthorityRepository;
 import com.newfit.reservation.domains.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -32,11 +33,11 @@ public class WebSecurityConfig {
 
     private final TokenProvider tokenProvider;
     private final UserRepository userRepository;
-    private final AuthorityRepository authorityRepository;
     private final RefreshTokenRepository refreshTokenRepository;
     private final OAuth2UserCustomService oAuth2UserCustomService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
     private final OAuth2FailureHandler oAuth2FailureHandler;
+    private final ObjectMapper objectMapper;
 
     // 누구나 접근할 수 있는 URI 패턴을 정의
     private static final String[] PERMIT_ALL_PATTERNS = new String[] {
@@ -64,6 +65,7 @@ public class WebSecurityConfig {
                     .requestMatchers(AntPathRequestMatcher.antMatcher("/api/**")).authenticated()
                     .anyRequest().permitAll())
                 .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(customExceptionHandlingFilter(), TokenAuthenticationFilter.class)
                 .oauth2Login(oauth -> oauth
                     .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(oAuth2UserCustomService))
                     .successHandler(oAuth2SuccessHandler)
@@ -81,12 +83,17 @@ public class WebSecurityConfig {
 
     @Bean
     public TokenAuthenticationFilter tokenAuthenticationFilter() {
-        return new TokenAuthenticationFilter(tokenProvider, refreshTokenRepository, userRepository, authorityRepository);
+        return new TokenAuthenticationFilter(tokenProvider, refreshTokenRepository, userRepository);
     }
 
     @Bean
     public OAuth2AuthorizationRequestCookieRepository oAuth2AuthorizationRequestCookieRepository() {
         return new OAuth2AuthorizationRequestCookieRepository();
+    }
+
+    @Bean
+    public CustomExceptionHandlingFilter customExceptionHandlingFilter() {
+        return new CustomExceptionHandlingFilter(objectMapper);
     }
 
     private String getToken(HttpServletRequest request) {
