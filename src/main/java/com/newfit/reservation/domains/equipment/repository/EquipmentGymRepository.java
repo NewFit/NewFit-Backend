8c2c7a3a5f3c73b647a3d4d6971de663303a26f2
chosw1002@naver.com <chosw1002@naver.com>
2023-11-04 14:51:37 +0900

    refactor: endpoint 변경에 따른 로직 수정 (#248)

--- src/main/java/com/newfit/reservation/domains/equipment/repository/EquipmentGymRepository.java
+++ src/main/java/com/newfit/reservation/domains/equipment/repository/EquipmentGymRepository.java
@@ -36,4 +36,18 @@ public interface EquipmentGymRepository extends JpaRepository<EquipmentGym, Long
     Optional<EquipmentGym> findAvailableByEquipmentIdAndStartAtAndEndAt(@Param("equipmentId") Long equipmentId,
                                                                         @Param("startAt") LocalDateTime startAt,
                                                                         @Param("endAt") LocalDateTime endAt);
+
+    @Query(value = "SELECT * " +
+            "FROM Equipment_Gym eg " +
+            "WHERE eg.id = :equipmentGymId" +
+            "  AND NOT EXISTS (" +
+            "    SELECT 1 FROM Reservation r" +
+            "    WHERE r.equipment_gym_id = eg.id AND " +
+            "        ((:startAt BETWEEN r.start_at AND r.end_at)" +
+            "        OR (:endAt BETWEEN r.start_at AND r.end_at)" +
+            "        OR (:startAt <= r.start_at AND r.end_at <= :endAt))) " +
+            "LIMIT 1;", nativeQuery = true)
+    Optional<EquipmentGym> findAvailableByEquipmentGymIdAndStartAtAndEndAt(@Param("equipmentGymId") Long equipmentGymId,
+                                                                           @Param("startAt") LocalDateTime startAt,
+                                                                           @Param("endAt") LocalDateTime endAt);
 }
