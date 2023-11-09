4b5e14f423daf42f1aed69060a7731e95d59cc1e
chosw1002@naver.com <chosw1002@naver.com>
2023-11-09 20:14:57 +0900

    feat: BusinessTime에 Builder 메서드 추가 (#254)

--- src/main/java/com/newfit/reservation/domains/gym/domain/BusinessTime.java
+++ src/main/java/com/newfit/reservation/domains/gym/domain/BusinessTime.java
@@ -2,10 +2,13 @@ package com.newfit.reservation.domains.gym.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import java.time.LocalTime;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BusinessTime {
 
     // 24시간 운영이면 openAt과 closeAt을 모두 00:00으로 설정
@@ -17,6 +20,21 @@ public class BusinessTime {
 
     @Column(nullable = false)
     private Boolean allDay;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private BusinessTime(LocalTime openAt, LocalTime closeAt, Boolean allDay) {
+        this.openAt = openAt;
+        this.closeAt = closeAt;
+        this.allDay = allDay;
+    }
+
+    public static BusinessTime create(LocalTime openAt, LocalTime closeAt, Boolean allDay) {
+        return BusinessTime.builder()
+                .openAt(openAt)
+                .closeAt(closeAt)
+                .allDay(allDay)
+                .build();
+    }
 
     // getAllDay가 아닌 isAllDay라는 이름을 사용하기 위해 @Getter 미사용
     public boolean isAllDay() {
