d78d5f72251cf4e570ada1ae42a91dad3eccc745
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2023-11-09 16:57:11 +0900

    refactor: 인증 로직 변경 (#252) (#253)
    * refactor: 인증/인가 로직 변경에 따라 불필요한 클래스 제거 (#252)
    
    * refactor: 인증/인가 로직 변경에 따라 불필요한 security config 제거 (#252)
    
    * feat: 변경된 로직에 따라 token 발행 로직 추가 (#252)
    
    - controller와 service 생성
    - dto 생성
    
    * feat: id의 종류 구분을 위한 enum 추가 (#252)
    
    * docs: oauth-client 제거 (#252)
    
    * refactor: notnull constraints를 notblank로 변경 (#252)

--- src/main/java/com/newfit/reservation/common/auth/config/WebSecurityConfig.java
+++ src/main/java/com/newfit/reservation/common/auth/config/WebSecurityConfig.java
@@ -3,10 +3,6 @@ package com.newfit.reservation.common.auth.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.newfit.reservation.common.auth.jwt.TokenAuthenticationFilter;
 import com.newfit.reservation.common.auth.jwt.TokenProvider;
-import com.newfit.reservation.common.auth.oauth.OAuth2AuthorizationRequestCookieRepository;
-import com.newfit.reservation.common.auth.oauth.OAuth2UserCustomService;
-import com.newfit.reservation.common.auth.oauth.handler.OAuth2FailureHandler;
-import com.newfit.reservation.common.auth.oauth.handler.OAuth2SuccessHandler;
 import com.newfit.reservation.common.exception.CustomExceptionHandlingFilter;
 import com.newfit.reservation.domains.auth.repository.RefreshTokenRepository;
 import com.newfit.reservation.domains.user.repository.UserRepository;
@@ -34,9 +30,6 @@ public class WebSecurityConfig {
     private final TokenProvider tokenProvider;
     private final UserRepository userRepository;
     private final RefreshTokenRepository refreshTokenRepository;
-    private final OAuth2UserCustomService oAuth2UserCustomService;
-    private final OAuth2SuccessHandler oAuth2SuccessHandler;
-    private final OAuth2FailureHandler oAuth2FailureHandler;
     private final ObjectMapper objectMapper;
 
     // 누구나 접근할 수 있는 URI 패턴을 정의
@@ -66,12 +59,6 @@ public class WebSecurityConfig {
                     .anyRequest().permitAll())
                 .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(customExceptionHandlingFilter(), TokenAuthenticationFilter.class)
-                .oauth2Login(oauth -> oauth
-                    .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(oAuth2UserCustomService))
-                    .successHandler(oAuth2SuccessHandler)
-                    .failureHandler(oAuth2FailureHandler)
-                    .authorizationEndpoint(authorizationEndpoint -> authorizationEndpoint
-                        .authorizationRequestRepository(oAuth2AuthorizationRequestCookieRepository())))
                 .logout(logout -> logout
                     .logoutUrl("/logout")
                     .addLogoutHandler((request, response, authentication) -> {
@@ -84,11 +71,6 @@ public class WebSecurityConfig {
     @Bean
     public TokenAuthenticationFilter tokenAuthenticationFilter() {
         return new TokenAuthenticationFilter(tokenProvider, refreshTokenRepository, userRepository);
-    }
-
-    @Bean
-    public OAuth2AuthorizationRequestCookieRepository oAuth2AuthorizationRequestCookieRepository() {
-        return new OAuth2AuthorizationRequestCookieRepository();
     }
 
     @Bean
