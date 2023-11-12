fe6bf143514f348a5f32c803c9515db081241eb9
chosw1002@naver.com <chosw1002@naver.com>
2023-11-12 21:54:37 +0900

    refactor: OAuth2SuccessHandler에서 admin의 요청만을 처리하도록 수정 (#256)
    - cookie를 이용해 JWT 토큰 전달

--- src/main/java/com/newfit/reservation/common/auth/oauth/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/newfit/reservation/common/auth/oauth/handler/OAuth2SuccessHandler.java
@@ -3,44 +3,41 @@ package com.newfit.reservation.common.auth.oauth.handler;
 import com.newfit.reservation.common.auth.jwt.TokenProvider;
 import com.newfit.reservation.common.auth.oauth.CustomOAuth2User;
 import com.newfit.reservation.domains.auth.domain.OAuthHistory;
-import com.newfit.reservation.domains.authority.domain.Authority;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import java.io.IOException;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
     private final TokenProvider tokenProvider;
+    
+    @Value("${admin.attributeName}")
+    private String attributeName;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
         OAuthHistory oAuthHistory = oAuth2User.getOAuthHistory();
-        if (oAuthHistory.isRegistered()) {  // 회원가입이 완료된 경우
-            Authority authority = oAuthHistory.getUser().getAuthorityList().stream().findAny().orElse(null);
-            String accessToken = tokenProvider.generateAccessToken(oAuthHistory.getUser());
-            String refreshToken = tokenProvider.generateRefreshToken(oAuthHistory.getUser());
 
-            response.setHeader("access-token", accessToken);
-            response.setHeader("refresh-token", refreshToken);
-            log.info("AfterSignup.accessToken = {}", accessToken);
-            if (authority != null) {    // 등록된 gym이 있는 경우
-                response.setHeader("authority-id", authority.getId().toString());
-            }
-            else {  // 등록된 gym이 없는 경우
-                response.setHeader("user-id", oAuthHistory.getUser().getId().toString());
-            }
-        } else {    // 회원가입이 미실시된 경우
-            String accessToken = tokenProvider.generateAccessToken(oAuthHistory.getUser());
-            log.info("BeforeSignup.accessToken = {}", accessToken);
-            response.setHeader("access-token", accessToken);
-            response.setHeader("oauth-history-id", oAuthHistory.getId().toString());
+        if (oAuthHistory.getAttributeName().equals(attributeName)) {
+            Cookie cookie = new Cookie("admin-token", tokenProvider.generateAdminToken());
+            cookie.setPath("/v1/admin");
+            response.addCookie(cookie);
+            response.sendRedirect("/v1/admin");
+        }
+        else {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.sendRedirect("/login");
         }
     }
 }
