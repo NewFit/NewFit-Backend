a872f8110e96f98a166311467c85f4a06eae20c7
hoyun06 <97820785+hoyun06@users.noreply.github.com>
2023-11-28 18:23:22 +0900

    issue #263,267 작업사항 개발 서버 반영 (#269)
    * refactor: 헬스장 전체 조회 시 response에 헬스장 id도 포함하도록 수정 (#263) (#264)
    
    * refactor: gym 패키지 소속 GymResponse에 gymId 필드 추가 (#263)
    
    * refactor: authority 패키지 소속 GymResponse에 gymId 필드 추가 (#263)
    
    * refactor: 불필요한 builder 제거 (#263)
    
    * refactor: Gym 조회 시 대소문자 구분하지 않고 조회할 수 있도록 수정 (#267) (#268)
    
    * refactor: case insensitive search를 위한 쿼리 수정 (#267)
    
    * test: case insensitive search DataJpaTest 테스트 (#267)

--- src/main/java/com/newfit/reservation/domains/authority/dto/response/GymResponse.java
+++ src/main/java/com/newfit/reservation/domains/authority/dto/response/GymResponse.java
@@ -3,9 +3,7 @@ package com.newfit.reservation.domains.authority.dto.response;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.newfit.reservation.domains.authority.domain.Authority;
-import com.newfit.reservation.domains.gym.domain.Gym;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,19 +11,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class GymResponse {
+    private Long gymId;
     private String gymName;
     private String address;
 
-    @Builder
-    private GymResponse(String gymName, String address) {
+    private GymResponse(Long gymId, String gymName, String address) {
+        this.gymId = gymId;
         this.gymName = gymName;
         this.address = address;
     }
 
     public GymResponse(Authority authority) {
-        this(authority.getGym().getName(), authority.getGym().getAddress());
+        this(authority.getGym().getId(), authority.getGym().getName(), authority.getGym().getAddress());
     }
-
-    // Gym 객체로부터 GymResponseDto를 생성합니다.
-    public GymResponse(Gym gym) { this(gym.getName(), gym.getAddress()); }
 }
