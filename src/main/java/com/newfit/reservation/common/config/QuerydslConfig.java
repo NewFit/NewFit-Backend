54539715ef9dcf5872fba835d9d0ce4a5a5a4026
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2023-11-29 15:35:35 +0900

    chore: querydsl 관련 config (#270) (#271)
    - querydsl 관련 dependency 추가
    - JPAQueryFactory를 빈으로 등록

---
+++ src/main/java/com/newfit/reservation/common/config/QuerydslConfig.java
@@ -0,0 +1,18 @@
+package com.newfit.reservation.common.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
