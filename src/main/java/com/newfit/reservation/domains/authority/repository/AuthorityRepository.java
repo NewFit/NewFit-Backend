f7ab41fde2931befc8516ccef88cb9a8d4a86fdd
Cho Sangwook <82208159+Sangwook02@users.noreply.github.com>
2023-12-01 14:12:12 +0900

    refactor: AuthorityRepository의 @Query문을 querydsl로 변경 (#272) (#273)
    * refactor: AuthorityRepository의 @Query문을 querydsl로 변경 (#272)
    
    * test: AuthorityRepository에 대한 test 작성 (#272)
    
    * fix: authorityId를 userId로 수정 (#272)

--- src/main/java/com/newfit/reservation/domains/authority/repository/AuthorityRepository.java
+++ src/main/java/com/newfit/reservation/domains/authority/repository/AuthorityRepository.java
@@ -1,26 +1,10 @@
 package com.newfit.reservation.domains.authority.repository;
 
 import com.newfit.reservation.domains.authority.domain.Authority;
-import com.newfit.reservation.domains.authority.domain.RoleType;
 import com.newfit.reservation.domains.gym.domain.Gym;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import java.util.List;
-import java.util.Optional;
 
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+public interface AuthorityRepository extends JpaRepository<Authority, Long>, AuthorityRepositoryCustom {
     List<Authority> findAllAuthorityByGym(Gym gym);
-
-    @Query("SELECT a FROM Authority a WHERE a.user.id = :userId")
-    List<Authority> findAllAuthorityByUserId(@Param("userId") Long id);
-
-    @Query("SELECT a from Authority a where a.user.id =:userId and a.gym.id =:gymId and a.roleType =:roleType")
-    Authority findOneByUserIdAndGymIdAndRoleType(@Param("userId") Long userId, @Param("gymId") Long gymId, @Param("roleType") RoleType roleType);
-
-    @Query("select a from Authority a where a.creditAcquisitionCount <> 0")
-    List<Authority> findAllByCreditAcquisitionCountNotZero();
-
-    @Query("SELECT a from Authority a where a.user.nickname =:nickname and a.gym.id =:gymId")
-    Optional<Authority> findOneByUserNicknameAndGymId(@Param("nickname") String nickname, @Param("gymId") Long gymId);
 }
