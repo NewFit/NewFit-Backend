efa1077281e3e37b3f10336b00440b7ed4b9c11b
chosw1002@naver.com <chosw1002@naver.com>
2023-12-13 18:21:26 +0900

    test: test 코드 생성 (#287)

---
+++ src/test/java/com/newfit/reservation/domains/credit/repository/CreditRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.newfit.reservation.domains.credit.repository;
+
+import com.newfit.reservation.common.config.QuerydslConfig;
+import com.newfit.reservation.domains.authority.domain.Authority;
+import com.newfit.reservation.domains.authority.repository.AuthorityRepository;
+import com.newfit.reservation.domains.credit.dto.response.CreditRanking;
+import com.newfit.reservation.domains.credit.dto.response.UserRankInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.time.LocalDateTime.now;
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@Import(QuerydslConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CreditRepositoryTest {
+
+    @Autowired
+    private CreditRepository creditRepository;
+
+    @Autowired
+    private AuthorityRepository authorityRepository;
+
+
+    @Test
+    void findTop10ByGymIdAndYearAndMonth() {
+        // given
+        Long gymId = 1L;
+        LocalDateTime now = now();
+
+        // when
+        List<CreditRanking> creditList = creditRepository
+                .findTop10ByGymIdAndYearAndMonth(gymId, (short) now.getYear(), (short) now.getMonthValue());
+
+        // then
+        assertThat(creditList).isNotNull();
+    }
+
+    @Test
+    void findRank() {
+        // given
+        Long authorityId = 1L;
+        Long gymId = 1L;
+        LocalDateTime now = now();
+        Authority authority = authorityRepository.findById(authorityId)
+                .orElseThrow(IllegalArgumentException::new);
+        // when
+        UserRankInfo userRankInfo = creditRepository
+                .findRank(authority.getId(),
+                        gymId,
+                        (short) now.getYear(),
+                        (short) now.getMonthValue())
+                .map(UserRankInfo::new)
+                .orElseGet(() -> UserRankInfo.noCredit(authority));
+
+        // then
+        assertThat(userRankInfo).isNotNull();
+    }
+}
